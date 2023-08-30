acf3c7c9c8a77e15c6bf2f41627813f713614c10
lcshenry <lucashenrique51513@gmail.com>
2023-08-30 01:47:48 -0300

    new items

---
+++ src/main/java/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,52 @@
+package main.java.map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+    agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if (!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+            if (numeroPorNome == null) {
+                System.out.println("Contato não encontrado na agenda.");
+            }
+        } else {
+            System.out.println("A agenda de contatos está vazia.");
+        }
+        return numeroPorNome;
+    }
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Lucas",9866606);
+        agendaContatos.adicionarContato("João",2866909);
+        agendaContatos.adicionarContato("Gabriel",2106909);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Gabriel");
+
+        agendaContatos.exibirContatos();
+
+        System.out.println("O numero é " + agendaContatos.pesquisarPorNome("João"));
+    }
+}
